200500e6f398aea0d01c14cf1c95e9c31077ecdc
rasel726 <md.rasel2726@gmail.com>
2022-08-10 02:31:38 +0600

    duplicate file added

---
+++ duplicate.js
@@ -0,0 +1,15 @@
+const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'babul', 'abul', 'kabul', 'gabul', 'cabul', 'babul', 'abul', 'abul'];
+
+function remoteDuplicate(names){
+    const unique = [] 
+    for(let i = 0; i < names.length; i++){
+        const name = names[i];
+        if(unique.includes(name) === false){
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+
+const uniqueNames = remoteDuplicate(names);
+console.log(uniqueNames);
